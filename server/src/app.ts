7a98aa2d39d4a64bf4375dda31b3f2711ae38c16
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-07-17 20:33:30 +0530

    allow origin cors url

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000',
+       'https://milk-delivery-platform-sjlc.vercel.app'
+  ],
   credentials: true,
 }))
 app.use(express.json());
